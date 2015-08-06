c60a82ec504b176e0b76358acca74a01f8798789
Adrian Clark <adrian@adrianclark.me>
2015-08-05 23:43:19 -0400

    continued tutorial work for C#, and played with some functionality in the language.

--- C_sharp_training/Program.cs
+++ C_sharp_training/Program.cs
@@ -1,5 +1,5 @@
 ﻿using System;
-using System.Math;
+using System.Text;
 
 namespace C_sharp_training
 {
@@ -7,21 +7,64 @@ namespace C_sharp_training
 	{
 		public static void Main (string[] args)
 		{
-			Multiplier ();
-		}
+			//Multiplier ();
+			//buildString ();
+			//parseTryer("32.339");
+			//getDateTime();
 
-		private static void Multiplier () 
-		{
-			int num01;
-			int num02;
-
-			Console.WriteLine ("Type a number to be multiplied Please!: ");
-			num01 = Convert.ToInt32(Console.ReadLine ());
-			Console.WriteLine ("What number do you want your number to be multiplied by?: ");
-			num02 = Convert.ToInt32(Console.ReadLine ());	
-			Console.WriteLine (num01 * num02);
+			Console.WriteLine ("You're doing good, though.");
+			Console.ReadLine ();
 		}
-			
+//
+//		public static void getDateTime()
+//		{
+//			//example of using a pre-selected date with DateTime object, and using
+//			//a mask to determine how to format the date to a string.
+//			DateTime dt = new DateTime(2015, 8, 5, 11, 21, 00);
+//			Console.WriteLine (dt.ToString("MMMM d, yyyy"));
+//
+//			//example of using DateTime methods to create a new date.
+//			DateTime anotherDay = dt.AddDays (6);
+//			Console.WriteLine (anotherDay.ToString("MMMM d, yyyy") + " is 6 days from dt date.");
+//
+//			//example of getting the actual currect DateTime info
+//			DateTime now = DateTime.Now;
+//			Console.WriteLine (now.ToString("yyyy, MMMM d"));
+//		}
+//
+//		private static void Multiplier () 
+//		{
+//			int num01;
+//			int num02;
+//
+//			Console.WriteLine ("Type a number to be multiplied Please!: ");
+//			num01 = Convert.ToInt32(Console.ReadLine ());
+//			Console.WriteLine ("What number do you want your number to be multiplied by?: ");
+//			num02 = Convert.ToInt32(Console.ReadLine ());	
+//			Console.WriteLine (num01 * num02);
+//		}
+//
+//		private static void buildString ()
+//		{
+//			StringBuilder makeString = new StringBuilder();
+//			makeString.Append ("hello")
+//					  .Append (" world.");
+//			Console.WriteLine (makeString.ToString());
+//		}
+//
+//		private static void parseTryer (string text)
+//		{
+//			double dubFromString;
+//			if (double.TryParse (text, out dubFromString)) 
+//			{
+//				Console.WriteLine ("Parsing worked, DubFromString is {0}", dubFromString); 
+//			} 
+//			else 
+//			{
+//				Console.WriteLine ("Parsing didn't work. You suck.");	
+//			}
+//		}
+//
 //		private static void Write (object input)
 //		{
 //			if (input.GetType() == typeof(string))
---
+++ ConsoleClassFun/ConsoleClassFun/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace ConsoleClassFun
+{
+	class MainClass
+	{
+		public static void Main (string[] args)
+		{	
+			Human Adrian = new Human (25, "Adrian Clark");
+			Console.WriteLine ("Thus we begin a great and brilliant story!");
+			Console.WriteLine ("We meet our hero, {0} at the foot of his bed, crying.", Adrian.name);
+			Console.WriteLine ("Do you ask him why he's crying <type ask> \n or do you walk over to him? <type comfort>");
+			string response = Console.ReadLine();
+
+			if (response == "ask") 
+			{
+				Console.WriteLine ("You walk over and ask him why he's crying...");
+				Console.WriteLine ("\"I'm honestly just bummed right now.\nYou'd think, I am {0} years old and I've never \neven seen the grand canyon. What the hell\"", Adrian.age);
+			} else 
+			{
+				Console.WriteLine ("ignant");
+			}
+
+			Console.ReadLine ();
+		}
+
+	}
+
+	public class Human
+	{
+		public int age;
+		public string name;
+		public Human (int ager, string namer)
+		{
+			age = ager;
+			name = namer;
+		}
+
+	}
+//	public class Male : Human
+//	{
+//		
+//	}
+
+}
